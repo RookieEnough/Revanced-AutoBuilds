4ead03110d23d5dde0aac4069eb38a17437f96bb
Sarthak Sinha <128968367+RookieEnough@users.noreply.github.com>
2026-01-22 13:49:23 +0530

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -21,7 +21,8 @@ def run_build(app_name: str, source: str, arch: str = "universal") -> str:
     download_methods = [
         downloader.download_apkmirror,
         downloader.download_apkpure,
-        downloader.download_uptodown
+        downloader.download_uptodown,
+        downloader.download_aptoide
     ]
 
     input_apk = None
